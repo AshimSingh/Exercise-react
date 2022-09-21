3e09699382f966c5a2f68db464cc87d2cc9fb061
Ashim Singh <ashimsingh948@gmail.com>
2022-09-21 22:32:39 +0545

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import {Box} from '@mui/material'
+import Navbar from './components/Navbar'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from "./pages/Home"
+import Exer from './pages/exercise'
+import Footer from './components/Footer'
+const App=()=>{
+    return(
+        <>
+        {/* // <div>
+        //     <BrowserRouter>
+        //             <h1>Hello mate</h1>
+        //             <Routes>
+        //                 <Route path="/" element={<Home/>}></Route>
+        //             </Routes>
+        //         </BrowserRouter>
+        // </div> */}
+            <BrowserRouter>
+                <Box> 
+                <Navbar></Navbar>
+                         <Routes>
+                        <Route path='/' element={<Home/>}></Route>
+                        <Route path='/exercise/:id' element={<Exer/>}></Route>
+                    </Routes>
+                <Footer></Footer> 
+                </Box> 
+            </BrowserRouter>
+        </>
+    )
 }
-
-export default App;
+export default App
---
+++ src/pages/exercise.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Exer=()=>{
+    return(
+        <>
+            <h1>Exercises</h1>        
+        </>
+    )
+}
+export default Exer
